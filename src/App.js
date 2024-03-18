03aeb7f1035dc51bd474c7445fc0107004cf8f73
Rahul kumar <rahulk07072000@gmail.com>
2024-03-18 17:23:40 +0530

    Suduko

--- src/App.js
+++ src/App.js
@@ -1,25 +1,152 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Box extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state =
+      {
+        count :1,
+        color: '#ccc',
+        disabled: false,
+        
+      
+      };
+    
+  }
+
+  checkAnswer(useranswer){
+    if(this.props.answer == useranswer)
+    {
+      this.setState(
+        {
+          color:'#ccc',
+          disabled: true
+        }
+      )
+
+    }
+   else if(this.props.answer !== useranswer && useranswer!=''){
+     this.props.countWrong();
+      if(this.state.count == 1)
+      {
+        this.setState(
+          {
+            color:'green',
+            count:2
+          }
+        )
+
+      }
+
+      else if(this.state.count == 2)
+      {
+        this.setState(
+          {
+            color:'yellow',
+            count:3
+          }
+        )
+
+      }
+
+      else if(this.state.count == 3)
+      {
+        this.setState(
+          {
+            color:'red',
+            count:4,
+            disabled :true
+          }
+        )
+
+      }
+   
+    }
+    
+     
+      }
+
+
+
+
+  render(){
+      return (
+        <div className='grid-box'>
+          {this.props.number ? (
+            <div className='Box' > {this.props.number}</div>
+          ) : (
+
+                  <input type='text' value={this.state.value} style={{backgroundColor:this.state.color}} 
+                  onChange={(event)=> this.checkAnswer(event.target.value)}  disabled={(this.state.disabled)? 'disabled':''} maxLength={1} />
+                )
+          }
+        </div>
+      )
+
+
+    }
+}
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      attempt: 0
+    }
+    this.incorrectAttempt = this.incorrectAttempt.bind(this)
+  }
+
+  incorrectAttempt(){
+    this.setState(
+      {
+        attempt:this.state.attempt+1
+      }
+    )
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="line">
+          <div className="Box1-row">
+
+            <Box number={3} />
+            <Box answer={1} countWrong={this.incorrectAttempt} />
+            <Box answer={4} countWrong={this.incorrectAttempt}/>
+            <Box number={2} />
+          </div>
+       
+          <div className="Box1-row">
+            <Box answer={2} countWrong={this.incorrectAttempt}/>
+            <Box number={4} />
+            <Box number={1} />
+            <Box answer={3} countWrong={this.incorrectAttempt}/>
+
+          </div>
+        </div>
+        <div className="line">
+          <div className="Box1-row">
+            <Box answer={1} countWrong={this.incorrectAttempt}/>
+            <Box number={3} />
+            <Box number={2} />
+            <Box answer={4} countWrong={this.incorrectAttempt}/>
+
+          </div>
+       
+          <div className="Box1-row">
+            <Box number={4} />
+            <Box answer={2} countWrong={this.incorrectAttempt}/>
+            <Box answer={3} />
+            <Box number={1} countWrong={this.incorrectAttempt}/>
+          </div>
+        </div>
+      <h1>Total incorrect Attempts : {this.state.attempt}</h1>
+      </div>
+
+    );
+  }
 }
 
 export default App;
